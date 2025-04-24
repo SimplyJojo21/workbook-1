c9d8a8999a1877c322e5908af2a8f9372a88a7ec
Joanna Akhagbeme <simply.jojo210@gmail.com>
2025-04-24 02:17:53 -0500

    completed ShoppingList app

---
+++ ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Apples, Bananas, Celery, Danish, Eggs, Fig Newtons, Grapes, Hazelnuts, Ice Cream, Jellybeans");
+    }
+}
